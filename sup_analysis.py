711b5d0b5fee1695df514805d9f90ea50ffa1be7
Saurav Dash <102579410+DashSaurav@users.noreply.github.com>
2022-07-24 17:14:52 +0530

    Update sup_analysis.py

--- sup_analysis.py
+++ sup_analysis.py
@@ -1,6 +1,15 @@
 import streamlit as st
-import pandas as pd
 import hydralit_components as hc
+import networkx as nx
+import networkx.algorithms.approximation as nx_app
+import math
+import numpy as np
+import pandas as pd
+import warnings
+from geopy.exc import GeocoderTimedOut
+from geopy.geocoders import Nominatim
+import plotly.express as px
+warnings.filterwarnings(action = "ignore")
 
 data = pd.read_csv("Superstore_sales_Data.csv")
 def sale_tot(Customer_name):
@@ -68,7 +77,7 @@ def date_sale_month(Customer_name):
 theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'blue','content_color': 'blue'}
 
 def ana():
-    sel = st.sidebar.radio("Select a Analysis Type",('By Name', 'By Products'))
+    sel = st.sidebar.radio("Select a Analysis Type",('By Name', 'By Products','Locations'))
 
     if sel=='By Name':
         col = st.columns(3)
@@ -136,3 +145,98 @@ def ana():
 
         st.write('Products Name List with Amount')
         st.write(prod_list(prod_name))
+
+    elif sel=='Locations':
+        sel_name = st.selectbox("Select Customer Name", data["Customer Name"].unique())
+
+        def loc_name(Customer_name):
+            df = data[data["Customer Name"] == Customer_name]
+            value = list(df["City"].unique())
+            return value
+
+        sel = loc_name(sel_name)
+        # st.write(sel)
+
+        coordinates = []
+        def findGeocode(city):
+            # try and catch is used to overcome
+            # the exception thrown by geolocator
+            # using geocodertimedout
+            try:	
+                # Specify the user_agent as your
+                # app name it should not be none
+                geolocator = Nominatim(user_agent="your_app_name")	
+                return geolocator.geocode(city)
+            except GeocoderTimedOut:	
+                return findGeocode(city)
+
+        for i in sel:
+            if findGeocode(i) != None:
+                loc = findGeocode(i)
+                coordinates.append((loc.longitude,loc.latitude))
+            else:
+                coordinates.append((np.nan,np.nan))
+
+        # st.write('Coordinates of Selected City:',coordinates)
+
+        G = nx.Graph()
+        #Create a graph object with number of nodes same as number of cities
+        nodes = np.arange(0, len(sel))
+        G.add_nodes_from(nodes)
+        #Create a dictionary of node and coordinate of each state for positions
+        positions = {node:coordinate for node, coordinate in zip(nodes, coordinates)}
+        #Create a dictionary of node and capital for labels
+        labels = {node:capital for node, capital in zip(nodes, sel)}
+
+        for i in nodes:
+            for j in nodes:
+                if i!=j:
+                    G.add_edge(i, j)
+
+        pos = {node:list(coordinate) for node, coordinate in zip(nodes, coordinates)}
+        # st.write(pos)
+
+        H = G.copy()
+        # Calculating the distances between the nodes as edge's weight.
+        for i in range(len(pos)):
+            for j in range(i + 1, len(pos)):
+                
+                #Multidimensional Euclidean distance from the origin to a point.
+                #euclidean distance between (x1, y1) and (x2, y2) is ((x2-x1)**2 + (y2-y1)**2)**0.5
+                dist = math.hypot(pos[i][0] - pos[j][0], pos[i][1] - pos[j][1])
+                dist = dist
+                G.add_edge(i, j, weight=dist)
+        cycle = nx_app.christofides(G, weight="weight")
+
+        edge_list = list(nx.utils.pairwise(cycle))
+
+
+        #Create a dictionary of node and capital for labels
+        labels = {node:capital for node, capital in zip(nodes, sel)}
+        tsp_cycle = [labels[value] for value in cycle]
+
+        df_map = {'City':tsp_cycle}
+        df_map = pd.DataFrame(df_map) 
+
+        longitude = []
+        latitude = []
+        for i in (df_map["City"]):
+            if findGeocode(i) != None:
+                loc = findGeocode(i)
+                latitude.append(loc.latitude)
+                longitude.append(loc.longitude)
+            else:
+                latitude.append(np.nan)
+                longitude.append(np.nan)
+        df_map["latitude"] = latitude
+        df_map["longitude"] = longitude
+        # st.write(df_map)
+        # st.map(df_map)
+        # px.set_mapbox_access_token(open(".mapbox_token").read())
+        fig = px.scatter_mapbox(df_map,
+                                lat=df_map.latitude,
+                                lon=df_map.longitude,
+                                hover_name="City",
+                                zoom=1)
+        fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=1, margin={"r":0,"t":0,"l":0,"b":0})
+        st.plotly_chart(fig, use_container_width=True)
